f2f712a58dbde4886828adf70d56e3fb513b6830
mattdaws <matthew.robert.daws@gmail.com>
2020-11-04 20:35:00 +0100

    Added js and css files to the project

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function sayMyName(name) {
+	// body...
+	alert('My name is: ' + name)
+}
+
+var car = {
+	make: "VW",
+	type: "Polo",
+	colour: "Blue",
+	isTurnedOn: false,
+	numberOfWheels: 4,
+	seats: ["seat 1", "seat 2", "seat 3", "seat 4"],
+	turnOn: function () {
+		this.isTurnedOn = true;
+	},
+	fly: function(){
+		alert('fly');
+	},
+	switchCar: function(isOn){
+		console.log('turn car ' + isOn)
+		if (isOn == true) {
+			this.isTurnedOn = true;
+		} else{
+			this.isTurnedOn = false;
+		}
+	}
+};
+
+console.log("hello there friends!");
